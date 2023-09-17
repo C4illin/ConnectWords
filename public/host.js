315885095d72e653dd9e99a8b4d39e1be0a66c8d
C4illin <emrik.ostling@gmail.com>
2023-09-17 13:48:05 +0200

    Added files

---
+++ public/host.js
@@ -0,0 +1,173 @@
+const socket = io("/host");
+
+// Get the session ID from the URL
+const sessionId = window.location.pathname.split('/')[2];
+
+// // Send the session ID to the server
+socket.emit('sessionId', sessionId);
+
+const sessionSocket = io("/" + sessionId);
+
+document.getElementById('session-id').innerText = sessionId
+
+// Generate the QR code for mobile users to join the session
+const qrCode = new QRCode(document.getElementById('qr-code'), {
+  text: window.location.href.replace('host', 'join'),
+  width: 256,
+  height: 256,
+  colorDark: '#000000',
+  colorLight: '#ffffff',
+  correctLevel: QRCode.CorrectLevel.H
+});
+
+var graph = {
+  nodes: [
+    { "name": "ÆØK", "width": 120, "height": 40, "color": "#54ce59", "fontColor": "#FFFFFF" },
+    { "name": "AØK", "width": 120, "height": 40, "color": "#cc0000", "fontColor": "#FFFFFF" },
+    { "name": "bØf", "width": 120, "height": 40, "color": "#3D7620", "fontColor": "#000000" },
+    { "name": "DNollK", "width": 120, "height": 40, "color": "#FA9907", "fontColor": "#FFFFFF" },
+    { "name": "EØK", "width": 120, "height": 40, "color": "#ffff00", "fontColor": "#000000" },
+    { "name": "FnollK", "width": 120, "height": 40, "color": "#000000", "fontColor": "#FFFFFF" },
+    { "name": "GØS", "width": 120, "height": 40, "color": "#efc26a", "fontColor": "#06516b" },
+    { "name": "HØK", "width": 120, "height": 40, "color": "#eb79ab", "fontColor": "#FFFFFF" },
+    { "name": "INollK", "width": 120, "height": 40, "color": "#9F36A1", "fontColor": "#FFFFFF" },
+    { "name": "KØK", "width": 120, "height": 40, "color": "#5c9143", "fontColor": "#FFFFFF" },
+    { "name": "MK", "width": 120, "height": 40, "color": "#034069", "fontColor": "#FFFFFF" },
+    { "name": "MnollK", "width": 120, "height": 40, "color": "#795548", "fontColor": "#FFFFFF" },
+    { "name": "NollKIT", "width": 120, "height": 40, "color": "#09cdda", "fontColor": "#FFFFFF" },
+    { "name": "SJØK", "width": 120, "height": 40, "color": "#1F2163", "fontColor": "#FFFFFF" },
+    { "name": "TBK", "width": 120, "height": 40, "color": "#EEEEEE", "fontColor": "#000000" },
+    { "name": "TDnollK", "width": 120, "height": 40, "color": "#8f1c43", "fontColor": "#FFFFFF" },
+    { "name": "VØK", "width": 120, "height": 40, "color": "#3d85c6", "fontColor": "#FFFFFF" },
+    { "name": "ZØK", "width": 120, "height": 40, "color": "#6E6E6E", "fontColor": "#FFFFFF" }
+  ],
+  links: [
+  ]
+};
+
+var width = window.innerWidth,
+  height = window.innerHeight;
+
+// var cola = cola.d3adaptor(d3)
+//   .linkDistance(140)
+//   .size([width, height]);
+
+const simulation = cola.d3adaptor(d3)
+  .nodes(graph.nodes)
+  .links(graph.links)
+  .size([width, height])
+  .linkDistance(140)
+  .start(200, 200, 200);
+
+  // .force("charge", d3.forceManyBody(-1000))
+  // .force("link", d3.forceLink().links(graph.links).distance(140))
+  // .force("center", d3.forceCenter(width / 2, height / 2))
+  // // .force("collide", d3.forceCollide().radius(60))
+  // // .force("x", d3.forceX(width / 2))
+  // // .force("y", d3.forceY(height / 2))
+  // .on("tick", ticked);
+
+var svg = d3.select("#wrapper").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// .append("g")
+// var pageBounds = { x: Number(window.innerWidth*0.3), y: 10, width: Number(width), height: Number(height) }
+// var realGraphNodes = graph.nodes.slice(0),
+//   fixedNode = { fixed: true, fixedWeight: 100 },
+//   topLeft = { ...fixedNode, x: 10, y: 10 },
+//   tlIndex = graph.nodes.push(topLeft) - 1,
+//   bottomRight = { ...fixedNode, x: 10 + pageBounds.width, y: 10 + pageBounds.height },
+//   brIndex = graph.nodes.push(bottomRight) - 1,
+//   constraints = [];
+
+// for (var i = 0; i < realGraphNodes.length; i++) {
+//   constraints.push({ axis: 'x', type: 'separation', left: tlIndex, right: i, gap: 100 });
+//   constraints.push({ axis: 'y', type: 'separation', left: tlIndex, right: i, gap: 100 });
+//   constraints.push({ axis: 'x', type: 'separation', left: i, right: brIndex, gap: 100 });
+//   constraints.push({ axis: 'y', type: 'separation', left: i, right: brIndex, gap: 100 });
+// }
+// .constraints(constraints)
+
+const node = svg.selectAll("rect")
+  .data(graph.nodes)
+  .enter().append("rect")
+  .attr("width", function (d) { return d.width; })
+  .attr("height", function (d) { return d.height; })
+  .attr("rx", 5).attr("ry", 5)
+  .style("fill", function (d) { return d.color; });
+
+var label = svg.selectAll("text")
+  .data(graph.nodes)
+  .enter().append("text")
+  .text(function (d) { return d.name; })
+  .style("fill", function (d) { return d.fontColor; })
+  .attr("class", "label");
+
+
+simulation.on("tick", function () {
+  node.transition().duration(1000)
+    .attr('x', function (d) { return d.x - d.width / 2; })
+    .attr('y', function (d) { return d.y - d.height / 2; })
+  
+  svg.selectAll("line")
+    .data(graph.links)
+    .join('line')
+    .attr('stroke-width', function (d) { return d.value * 2; })
+    .attr('stroke', '#999')
+    .lower().transition().duration(1000)
+    .attr('x1', function (d) { return d.source.x; })
+    .attr('y1', function (d) { return d.source.y; })
+    .attr('x2', function (d) { return d.target.x; })
+    .attr('y2', function (d) { return d.target.y; })
+  
+  label.transition().duration(1000)
+    .attr('x', function (d) { return d.x; })
+    .attr('y', function (d) { return d.y + d.height / 4; })
+});
+
+var addTestConnection = () => {
+  // between two random nodes
+  graph.links.push({ source: Math.floor(Math.random() * graph.nodes.length), target: Math.floor(Math.random() * graph.nodes.length), value: 1 });
+  simulation.links(graph.links);
+  simulation.start(200, 200, 200);
+}
+
+var nameToIndex = (name) => {
+  for (var i = 0; i < graph.nodes.length; i++) {
+    if (graph.nodes[i].name === name) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+sessionSocket.on('word', (data) => {
+  console.log(data);
+  var word1 = nameToIndex(data.word1);
+  var word2 = nameToIndex(data.word2);
+
+  if (word1 === -1 || word2 === -1) {
+    return;
+  }
+
+  var linkIndex = -1;
+  for (var i = 0; i < graph.links.length; i++) {
+    var link = graph.links[i];
+    if ((link.source.name == data.word1 && link.target.name == data.word2) || (link.source.name == data.word2 && link.target.name == data.word1)) {
+      linkIndex = i;
+      break;
+    }
+  }
+
+  if (linkIndex === -1) {
+    console.log('new link');
+    graph.links.push({ source: word1, target: word2, value: data.strength });
+  } else {
+    console.log('existing link');
+    graph.links[linkIndex].value += data.strength;
+  }
+
+  simulation.links(graph.links);
+  simulation.start(200, 200, 200);
+});
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Store the current session IDs
+const sessionIds = [];
+
+app.use(cors());
+app.use("/", express.static("public"));
+
+app.get('/host', (req, res) => {
+  // generate session id and send to /host/:sessionId
+  // sessionID is four numbers as string with leading zeros
+  // e.g. 0001, 0002, 0003, etc.
+  let sessionId = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+  while (sessionIds.includes(sessionId)) {
+    sessionId = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+  }
+  sessionIds.push(sessionId);
+  res.redirect(`/host/${sessionId}`);
+});
+
+// Serve the HTML file and static assets for hosting sessions
+app.get('/host/:sessionId', (req, res) => {
+  res.sendFile(__dirname + '/public/host.html');
+});
+
+io.of('/host').on('connection', (socket) => {
+  console.log('New client connected to host');
+
+  // Handle new session ID data
+  socket.on('sessionId', (sessionId) => {
+    // Join the specified session namespace
+    socket.join(sessionId);
+    console.log(`Host joined session ${sessionId}`);
+
+    // Get the namespace with the same name as the session ID
+    const sessionNamespace = io.of(`/${sessionId}`);
+
+    // Listen for the 'word' event on the session namespace
+    sessionNamespace.on('word', (data) => {
+      console.log(data);
+
+      // Emit the 'word' event with the data to the host namespace
+      io.of('/host').emit('word', data);
+    });
+  });
+
+  // Handle socket disconnections
+  socket.on('disconnect', () => {
+    console.log('Client disconnected from host');
+  });
+});
+
+app.get('/join', (req, res) => {
+  // get sessionId = req.query.sessionId
+  // if sessionId is in sessionIds, redirect to /join/:sessionId
+  // else show message that session does not exist
+  const { sessionId } = req.query;
+  if (sessionIds.includes(sessionId)) {
+    res.redirect(`/join/${sessionId}`);
+  } else {
+    res.send('Session does not exist');
+  }
+});
+
+// Serve the HTML file and static assets for joining sessions
+app.get('/join/:sessionId', (req, res) => {
+  res.sendFile(__dirname + '/public/join.html');
+});
+
+// Handle new socket connections for joining sessions
+io.of('/join').on('connection', (socket) => {
+  console.log('New client connected to join');
+
+  socket.on('word', (data) => {
+    console.log(data);
+
+    // if (!sessionIds.includes(data.sessionId)) {
+    //   console.log('Session does not exist');
+    //   return;
+    // }
+
+    // emit word data to host namespace
+    io.of('/'+data.sessionId).emit('word', data);
+  });
+
+  // Handle new session ID data
+  socket.on('sessionId', (sessionId) => {
+    // Join the specified session namespace
+    socket.join(sessionId);
+    console.log(`Client joined session ${sessionId}`);
+  });
+
+  // Handle socket disconnections
+  socket.on('disconnect', () => {
+    console.log('Client disconnected from join');
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
